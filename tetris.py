91a66c727bb3efe31ce2e3ad973309598c6e6374
Andreas Zeidler <andreas.zeidler@zeit.de>
2023-05-15 23:35:19 +0200

    make imports more compact

--- tetris.py
+++ tetris.py
@@ -1,7 +1,4 @@
-from curses import wrapper, KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN
-from curses import color_pair, init_pair, COLOR_GREEN, COLOR_BLACK
-from time import time
-from random import choice
+import curses as crs, random, time
 
 
 def put(board, tile, shape, x, y, four=(0, 1, 2, 3)):
@@ -11,29 +8,29 @@ def put(board, tile, shape, x, y, four=(0, 1, 2, 3)):
 
 
 def main(stdscr):
-    init_pair(1, COLOR_GREEN, COLOR_BLACK)
+    crs.init_pair(1, crs.COLOR_GREEN, crs.COLOR_BLACK)
     indent = stdscr.getmaxyx()[1] - 24 >> 1
-    stdscr.addstr(20, indent, '<!' + '=' * 20 + '!>', color_pair(1))
-    stdscr.addstr(21, indent, '  ' + '\\/' * 10, color_pair(1))
+    stdscr.addstr(20, indent, '<!' + '=' * 20 + '!>', crs.color_pair(1))
+    stdscr.addstr(21, indent, '  ' + '\\/' * 10, crs.color_pair(1))
     stdscr.timeout(10)
     board = [0xe007] * 21 + [0x1ff8] * 3
     tiles = 0x444400f0444400f0, 0x64400e2044c08e0, 0x88c00e80c4402e0, \
         0x46206c0046206c0, 0x2640c6002640c60, 0x46400e404c404e0, 0x660066006600660
     while board := [0xe007] * board.count(0xffff) + [l for l in board if l != 0xffff]:
-        tile, shape, y, x = choice(tiles), 0, 0, 6
-        stop = time() + (delay := 0.2)
+        tile, shape, y, x = random.choice(tiles), 0, 0, 6
+        stop = time.time() + (delay := 0.2)
         while updated := put(board, tile, shape, x, y):
             for row, line in enumerate(updated[1:-3]):
                 line = f'{line:>014b}'[3:-3].replace('1', '[]').replace('0', ' .')
-                stdscr.addstr(row, indent, f'<!{line}!>', color_pair(1))
+                stdscr.addstr(row, indent, f'<!{line}!>', crs.color_pair(1))
             key = stdscr.getch()
-            if key == KEY_LEFT and put(board, tile, shape, x + 1, y):
+            if key == crs.KEY_LEFT and put(board, tile, shape, x + 1, y):
                 x += 1
-            if key == KEY_RIGHT and put(board, tile, shape, x - 1, y):
+            if key == crs.KEY_RIGHT and put(board, tile, shape, x - 1, y):
                 x -= 1
-            if key == KEY_UP and put(board, tile, shape + 1, x, y):
+            if key == crs.KEY_UP and put(board, tile, shape+1, x, y):
                 shape += 1
-            if (elapsed := time() > stop) or key == KEY_DOWN:
+            if (elapsed := time.time() > stop) or key == crs.KEY_DOWN:
                 if not put(board, tile, shape, x, y + 1):
                     board = updated
                     break
@@ -41,4 +38,4 @@ def main(stdscr):
                 stop += delay if elapsed else 0
 
 
-wrapper(main)
+crs.wrapper(main)
