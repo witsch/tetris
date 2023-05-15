53d1df63fb967a8e8c6c1d80d859135729993227
Andreas Zeidler <andreas.zeidler@zeit.de>
2023-05-15 23:22:05 +0200

    use shorter (variable) name for the tile's variant

--- tetris.py
+++ tetris.py
@@ -9,8 +9,8 @@ tiles = (0x444400f0444400f0, 0x64400e2044c08e0, 0x88c00e80c4402e0, 0x46206c00462
          0x02640c6002640c60, 0x046400e404c404e0, 0x0660066006600660)
 
 
-def put(board, tile, orientation, x, y, four=(0, 1, 2, 3)):
-    masks = [(tile >> ((orientation % 4 << 4) + 4 * l) & 0xf) << x for l in four]
+def put(board, tile, shape, x, y, four=(0, 1, 2, 3)):
+    masks = [(tile >> ((shape % 4 << 4) + 4 * l) & 0xf) << x for l in four]
     if sum((masks[l] & board[y + l] for l in four)) == 0:
         return board[:y] + [masks[l] | board[y + l] for l in four] + board[y + 4:]
 
@@ -23,21 +23,21 @@ def main(stdscr):
     stdscr.timeout(10)
     board = start
     while board := [0xe007] * board.count(0xffff) + [l for l in board if l != 0xffff]:
-        tile, orientation, y, x = choice(tiles), 0, 0, 6
+        tile, shape, y, x = choice(tiles), 0, 0, 6
         stop = time() + (delay := 0.2)
-        while updated := put(board, tile, orientation, x, y):
+        while updated := put(board, tile, shape, x, y):
             for row, line in enumerate(updated[1:-3]):
                 line = f'{line:>014b}'[3:-3].replace('1', '[]').replace('0', ' .')
                 stdscr.addstr(row, indent, f'<!{line}!>', color_pair(1))
             key = stdscr.getch()
-            if key == KEY_LEFT and put(board, tile, orientation, x + 1, y):
+            if key == KEY_LEFT and put(board, tile, shape, x + 1, y):
                 x += 1
-            if key == KEY_RIGHT and put(board, tile, orientation, x - 1, y):
+            if key == KEY_RIGHT and put(board, tile, shape, x - 1, y):
                 x -= 1
-            if key == KEY_UP and put(board, tile, orientation + 1, x, y):
-                orientation += 1
+            if key == KEY_UP and put(board, tile, shape + 1, x, y):
+                shape += 1
             if (elapsed := time() > stop) or key == KEY_DOWN:
-                if not put(board, tile, orientation, x, y + 1):
+                if not put(board, tile, shape, x, y + 1):
                     board = updated
                     break
                 y += 1
