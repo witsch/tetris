866a7342316d6cecd20e2d756423dfa996c230ad
Andreas Zeidler <andreas.zeidler@zeit.de>
2023-05-16 00:08:55 +0200

    save the current board after it was checked (to avoid checking twice)

--- tetris.py
+++ tetris.py
@@ -13,24 +13,22 @@ def main(stdscr):
     stdscr.addstr(20, indent, '<!' + '=' * 20 + '!>', crs.color_pair(1))
     stdscr.addstr(21, indent, '  ' + '\\/' * 10, crs.color_pair(1))
     stdscr.timeout(10)
-    board = [0xe007] * 21 + [0x1ff8] * 3
+    board = save = [0xe007] * 21 + [0x1ff8] * 3
     tiles = 0x444400f0444400f0, 0x64400e2044c08e0, 0x88c00e80c4402e0, \
         0x46206c0046206c0, 0x2640c6002640c60, 0x46400e404c404e0, 0x660066006600660
-    while board := [0xe007] * board.count(0xffff) + [l for l in board if l != 0xffff]:
+    while board := [0xe007] * save.count(0xffff) + [l for l in save if l != 0xffff]:
         tile, shape, y, x = random.choice(tiles), 0, 0, 6
         stop = time.time() + (delay := 0.2)
         while updated := put(board, tile, shape, x, y):
             for row, line in enumerate(updated[1:-3]):
                 line = f'{line:>014b}'[3:-3].replace('1', '[]').replace('0', ' .')
                 stdscr.addstr(row, indent, f'<!{line}!>', crs.color_pair(1))
-            key = stdscr.getch()
+            key, save = stdscr.getch(), updated
             x += 1 if key == crs.KEY_LEFT and put(board, tile, shape, x+1, y) else 0
             x -= 1 if key == crs.KEY_RIGHT and put(board, tile, shape, x-1, y) else 0
             shape += 1 if key == crs.KEY_UP and put(board, tile, shape+1, x, y) else 0
             y += 1 if (elapsed := time.time() > stop) or key == crs.KEY_DOWN else 0
             stop += delay if elapsed else 0
-            if not put(board, tile, shape, x, y):
-                board = updated
 
 
 crs.wrapper(main)
