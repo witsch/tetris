6a2feb81e86c99d490b6ee01db216cf0c6d63f39
Andreas Zeidler <andreas.zeidler@zeit.de>
2023-05-15 23:28:07 +0200

    move board initialization and tiles definition

--- tetris.py
+++ tetris.py
@@ -4,11 +4,6 @@ from time import time
 from random import choice
 
 
-start = [0xe007] * 21 + [0x1ff8] * 3
-tiles = (0x444400f0444400f0, 0x64400e2044c08e0, 0x88c00e80c4402e0, 0x46206c0046206c0,
-         0x02640c6002640c60, 0x046400e404c404e0, 0x0660066006600660)
-
-
 def put(board, tile, shape, x, y, four=(0, 1, 2, 3)):
     masks = [(tile >> ((shape % 4 << 4) + 4 * l) & 0xf) << x for l in four]
     if sum((masks[l] & board[y + l] for l in four)) == 0:
@@ -21,7 +16,9 @@ def main(stdscr):
     stdscr.addstr(20, indent, '<!' + '=' * 20 + '!>', color_pair(1))
     stdscr.addstr(21, indent, '  ' + '\\/' * 10, color_pair(1))
     stdscr.timeout(10)
-    board = start
+    board = [0xe007] * 21 + [0x1ff8] * 3
+    tiles = 0x444400f0444400f0, 0x64400e2044c08e0, 0x88c00e80c4402e0, \
+        0x46206c0046206c0, 0x2640c6002640c60, 0x46400e404c404e0, 0x660066006600660
     while board := [0xe007] * board.count(0xffff) + [l for l in board if l != 0xffff]:
         tile, shape, y, x = choice(tiles), 0, 0, 6
         stop = time() + (delay := 0.2)
